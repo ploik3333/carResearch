c1b08da61225aee6d5a64f2446c87c1b84f9cd02
ploik3333 <ploik3333@gmail.com>
2026-01-17 14:45:55 -0500

    .

--- utila.py
+++ utila.py
@@ -1,32 +1,37 @@
+#TODO: compile all code into a nice module
 import json
-from typing import Tuple, Generator, Any, overload
 import math
-import pandas as pd
-from pathlib import Path
 import os
+from pathlib import Path
+from typing import Tuple, Generator
 
-# **depreciated**
+import numpy as np
+import pandas as pd
+
+
 # process each line of 'data' from the file,
+@DeprecationWarning
 def process_data(x: str) -> list[float , str, ...]:
     split = x.strip().split(",")
     return [float(split[0]), *split[1:]] # force first value to a float (the time) for later use
 
-
+def getFiles(dir:str, filetype:str = ".csv") -> list[Path]:
+    if not Path(dir).exists() and Path(dir).is_dir():
+        os.mkdir(dir)
+    return [Path(dir) / file for file in os.listdir(dir) if Path(file).suffix == filetype]
 # read data from file & calculate time differences
 def read(path: str | Path) -> Tuple[list[float],list[str]]:
     df = pd.read_csv(path)
     times = df['timestamp']
-    timediffs = [0] + [float(times[a] - times[a-1]) for a in range(1,len(times))]
+    timediffs = [0] + [*np.diff(np.array(times))] # [0] + [float(times[a] - times[a-1]) for a in range(1,len(times))]
     if 'attack' in df:
         attack = list(df['attack'])
     else:
         attack = None
-    return timediffs, attack
-
+    return timediffs, attack, times
 
 # useful function I wrote, like 'range', but returns windows, aka [(0,2),(2,4),(4,6)] for indexing
-# technically works with floats, but cant use float index
-def window(*args: list[int | float], include_extra: bool = True) -> Generator[tuple[int | float, int | float], None, None]:
+def window(*args: int | float, include_extra: bool = True) -> Generator[tuple[int | float, int | float], None, None]:
     boundCalc = math.ceil if include_extra else math.floor
 
     def _window(start: int | float, end: int | float, size: int | float, include_extra: bool) -> Generator[tuple[int | float, int | float], None, None]:
@@ -40,19 +45,26 @@ def window(*args: list[int | float], include_extra: bool = True) -> Generator[tu
         return _window(0, *args, include_extra=include_extra)
     raise NotImplementedError("Use implemented parameters: (start, end, size) OR (end, size)")
 
-
+#TODO generalize cache wrapper to use for any function
+#TODO FIX CACHE for simple vs not outputs
 def calc_cache(func):
     def wrapper(file, *args, cache = True, **kwargs):
-        cached_dir = Path("./cache") / (file.stem + ".json")
-        if cached_dir.exists() and cache:
+        if not isinstance(file, Path):
+            file = Path(file)
+
+        cached_dir = Path("./cache")
+        if cache and not cached_dir.exists():
+            os.mkdir(cached_dir)
+        cached_file_path = cached_dir / (file.stem + ".json")
+        if cached_file_path.exists() and cache:
             print(f"Retrieved data for {file.name}")
-            with open(cached_dir) as f:
+            with open(cached_file_path) as f:
                 return json.load(f)
         else:
             print(f"Calculating data for {file.name}")
             data = func(file, *args, **kwargs)
             if cache:
-                with open(cached_dir, "w") as fout:
+                with open(cached_file_path, "w") as fout:
                     json.dump(data, fout)
             return data
     return wrapper
